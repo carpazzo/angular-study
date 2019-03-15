acc569ed578d98545c98485ca20e2670ea4a8ebb
carpazzo <dalton_ramiro@hotmail.com>
2019-03-15 19:53:22 +0100

    two way bidings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {CoursesComponent} from './courses.component'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
@@ -16,7 +16,8 @@ import { CoursesService } from './courses.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     CoursesService,
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,5 +1,6 @@
 import { CoursesService } from './courses.service';
 import {Component} from '@angular/core';
+import { EventManagerPlugin } from '@angular/platform-browser/src/dom/events/event_manager';
 
 @Component({
    selector: 'courses', //like css selection.  //getTitle() use that inse de braces(string interpolation) is you want to use as a function, you can also concatnate using the plus +
@@ -11,8 +12,36 @@ import {Component} from '@angular/core';
             {{course}}
             </li>
         </ul>
+        <p>the class active is added to the button dynamicaly based in some condition </p> 
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
 
-        <button class="btn btn-primary">Save</button>
+        <p>the stylebinding also can be change with a condition </p> 
+        <button [style.backgroundColor]="isActive ?'red': 'white'">Test</button>
+
+        <p>For attribut biding use attr.name</p>
+
+        <p>event biding, use stop propagation to avoid event bubling</p>
+        <button (click)="onTest($event)">Test</button>
+
+        <div class="circle" (click)="onDivClicked()">
+            <button (click)="onTest($event)">Test</button>
+        </div>
+
+        <input (keyup.enter)="onKeyUp()"/>
+
+        <p>working with user input get the variable under type your test email</p>
+        
+        <input #email (keyup.enter)="onKeyUp(email.value)"/>
+
+        <p>new aproach for property biding old way..</p>
+
+        <input [value]="newemail" (keyup.enter)="newAproach()"/>
+
+        <p>2 ways biding syntax</p>
+        <input [(ngModel)]="newemail" (keyup.enter)="newAproach()"/>
+
+
+        
     `,
 })
 
@@ -21,10 +50,26 @@ export class CoursesComponent{
 
     title ="List of Courses";
     courses;
+    isActive = false;
+    newemail = "me@email.com";
     
     constructor(service: CoursesService){
         // let service = new CoursesService();
         this.courses = service.getCourses();
+    }
+    onTest($event){
+        $event.stopPropagation();
+        console.log("Test button clicked",$event);
+    }
+    onDivClicked(){
+        console.log("Div was clicked");
+    }
+    onKeyUp(email){
+        console.log(email);
+    }
+
+    newAproach(){
+        console.log(this.newemail);
     }
     //getTitle(){ 
        //return this.title;
